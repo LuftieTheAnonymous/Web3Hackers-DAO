86527d2ced1b6beab4f81b9e0e776a3435b969ea
Luftjunkie19 <luftjunkie@protonmail.com>
2025-12-15 11:40:19 +0100

    Done: Implement Split in Testing and Deploying files + Seclude Custom Option logic from Standard for votings. To fix: Comment the code, seek + fix vulnerabilities or bugs

--- apps/dao-contracts/src/governor/interaction-contracts/StandardGovernor.sol
+++ apps/dao-contracts/src/governor/interaction-contracts/StandardGovernor.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
 
+pragma solidity ^0.8.24;
+
 import {
     GovernorBase
 } from "../GovernorBase.sol";
-
-pragma solidity ^0.8.24;
 
 contract StandardGovernor is GovernorBase{
     enum StandardProposalVote {
@@ -46,15 +46,14 @@ constructor(address govTokenAddr) GovernorBase(govTokenAddr){}
 function createProposal(
         string calldata description,
         address[] memory targets,
-        uint256[] memory values,
         bytes[] memory calldatas,
         UrgencyLevel urgencyLevel,
         uint256 endBlockTimestamp,
         uint256 proposalTimelock,
         uint256 delayInSeconds
     ) external nonReentrant isElligibleToPropose override(GovernorBase) returns (bytes32)
-     { 
-    bytes32 proposalId=this.createProposal(description, targets, values, calldatas, urgencyLevel, endBlockTimestamp, proposalTimelock,delayInSeconds);
+     {
+    bytes32 proposalId=this.createProposal(description, targets, calldatas, urgencyLevel, endBlockTimestamp, proposalTimelock,delayInSeconds);
     votesOptions[proposalId][StandardProposalVote.Yes]= VoteOption(false, true);
     votesOptions[proposalId][StandardProposalVote.Abstain]= VoteOption(false, false);
     votesOptions[proposalId][StandardProposalVote.No]= VoteOption(true, false);
@@ -73,6 +72,41 @@ function createProposal(
 
     return votes;
 }
+
+    function castVote(
+        bytes32 proposalId,
+        string calldata reason,
+        address delegatee,
+        StandardProposalVote voteOptionIndex
+    ) external nonReentrant
+    isVotingActive(proposalId)
+    isElligibleToVote(proposalId)
+     {
+
+        uint256 weight = govToken.getPastVotes(msg.sender, block.number - 1);
+
+  Vote memory vote=Vote({
+            voterAddress:msg.sender,
+            delegatee:delegatee,
+            weight:weight,
+            voteOption: uint8(voteOptionIndex),
+            votedProposalId:proposalId,
+            isDelegated: delegatee != address(0),
+            reason:reason,
+            timestamp:block.timestamp
+        });
+
+
+        proposalVotes[proposalId][msg.sender] = vote;
+        proposalVoters[proposalId].push(msg.sender);
+        userVotes[msg.sender].push(vote);
+        
+        userVotedCount[msg.sender]++;
+        emit ProposalVoted(proposalId, msg.sender, weight);
+
+    }
+
+
 
 
 function succeedProposal(bytes32 proposalId) external onlyActionsManager isProposalReadyToSucceed(proposalId) nonReentrant { 
@@ -97,4 +131,40 @@ function succeedProposal(bytes32 proposalId) external onlyActionsManager isPropo
     proposals[proposalId].defeated = true;
     emit ProposalDefeated(proposalId, proposals[proposalId].proposer, block.timestamp);
 }
+
+function callProposal(Proposal memory proposal) internal onlyActionsManager nonReentrant {
+     for(uint i = 0; i < proposal.targets.length; i++){
+             address target = proposal.targets[i];
+             bytes memory data = proposal.calldatas[i];
+
+             if(target != address(0)){
+                 (bool success, bytes memory returnedData) = target.call(data);
+                 if(!success){
+                    if (returnedData.length > 0) { assembly { revert(add(returnedData,32), mload(returnedData)) } }
+                     revert ExecutionFailed();
+                 }
+                    emit CalldataExecuted();
+             }
+}
+        proposals[proposal.id].state = ProposalState.Executed;
+        proposals[proposal.id].executedAtBlockNumber = block.number;
+        proposals[proposal.id].executed = true;
+
+        emit ProposalExecuted(proposal.id);
+}
+
+
+function executeProposal(bytes32 proposalId) external onlyActionsManager nonReentrant {
+Proposal memory proposal = proposals[proposalId];
+
+    if(proposal.state != ProposalState.Queued && proposal.queuedAtBlockNumber + proposal.timelockBlockNumber > block.number){
+        revert InvalidProposalState();
+    }
+
+        callProposal(proposal);
+
+}
+
+
+
 }
